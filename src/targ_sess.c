e07d4b28a35effbdc4667decb6da60fcaf2e8f2c
hu gang <hugang@soulinfo.com>
2011-10-15 23:29:22 +0800

    change to raid45 for test

--- src/targ_sess.c
+++ src/targ_sess.c
@@ -113,7 +113,8 @@ targ_sess_t *targ_sess_new(const char *wwpn, void *data)
 	/* TODO more device support */
 	sess->dev.nr = 0;
 	sess->dev.array = kzalloc(sizeof(struct targ_dev)*32, GFP_ATOMIC);
-	dm_table_for_each(targ_sess_dev_assign, "linear", sess);
+	/*dm_table_for_each(targ_sess_dev_assign, "linear", sess);*/
+	dm_table_for_each(targ_sess_dev_assign, "raid45", sess);
 out:
 	return sess;
 }
