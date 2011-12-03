843202b70bc47c8511effa829e9924043b1164f6
hu gang <hugang@soulinfo.com>
2011-12-03 13:29:54 +0800

    adding base cache

---
+++ src/cache.c
@@ -0,0 +1,65 @@
+#include "qp_port.h"
+#include "qp_lsa.h"
+
+typedef struct CacheTag {
+	QActive super;
+	
+	struct list_head head;
+} Cache;
+
+static QState Cache_initial(Cache *me, QEvent const *e);
+static QState Cache_final  (Cache *me, QEvent const *e);
+static QState Cache_idle   (Cache *me, QEvent const *e);
+
+static Cache l_cache;
+
+QActive * const AO_Cache = (QActive *)&l_cache;
+
+/*..........................................................................*/
+void Cache_ctor(void)
+{
+	Cache *me = &l_cache;
+	QActive_ctor(&me->super, (QStateHandler)(Cache_initial));
+	INIT_LIST_HEAD(&me->head);
+}
+
+/* HSM definition ----------------------------------------------------------*/
+/*..........................................................................*/
+QState Cache_initial(Cache *me, QEvent const *e)
+{
+	(void *)e;    /* avoid the "unreferenced parameter" warning */
+
+	QActive_subscribe((QActive *)me, CACHE_WRITE_SIG);
+	QActive_subscribe((QActive *)me, CACHE_READ_SIG);
+
+	QS_OBJ_DICTIONARY(&l_cache);
+	QS_OBJ_DICTIONARY(&l_cache.head);
+	
+	QS_FUN_DICTIONARY(&Cache_initial);
+	QS_FUN_DICTIONARY(&Cache_final);
+	QS_FUN_DICTIONARY(&Cache_idle);
+	
+	QS_SIG_DICTIONARY(CACHE_WRITE_SIG, &l_cache);
+	QS_SIG_DICTIONARY(CACHE_READ_SIG,  &l_cache);
+	
+	return Q_TRAN(&Cache_idle);
+}
+/*..........................................................................*/
+QState Cache_final(Cache *me, QEvent const *e)
+{
+	switch (e->sig) {
+	case Q_ENTRY_SIG:
+		QActive_stop(&me->super);
+		return Q_HANDLED();
+	}
+	return Q_SUPER(&QHsm_top);
+}
+/*..........................................................................*/
+QState Cache_idle(Cache *me, QEvent const *e)
+{
+	switch (e->sig) {
+	case TERMINATE_SIG:
+		return Q_TRAN(&Cache_final);
+	}
+	return Q_SUPER(&QHsm_top);
+}
--- src/qp_lsa.h
+++ src/qp_lsa.h
@@ -3,8 +3,15 @@
 
 enum QP_LSA_Signals {
 	TERMINATE_SIG = Q_USER_SIG,
+	
+	CACHE_WRITE_SIG,
+	CACHE_READ_SIG,
 
 	MAX_SIG,
 };
+
+void Cache_ctor(void);
+
+extern QActive * const AO_Cache;
 
 #endif
